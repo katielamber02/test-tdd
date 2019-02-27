2051a72f6af7869b7f2d75f7cc7ce8988b9ea0fb
katielamber02 <32227217+katielamber02@users.noreply.github.com>
2019-02-27 13:39:33 +0200

    no message

--- src/App.test.js
+++ src/App.test.js
@@ -24,5 +24,5 @@ it("adds a new gift to `state` when clicking the add new button", () => {
 });
 it("adds a new gift to the rendered list when clicking the `add gift` button", () => {
   app.find(".btn-add").simulate("click");
-  expect(app.find(".gift-list").children().length).toEqual(1);
+  expect(app.find(".gift-list").children().length).toEqual(2);
 });
